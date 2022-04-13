b6d950f8708ad349be2eb104d994f998be277c49
Tiffceet <zkang.loo@gmail.com>
2022-04-13 23:25:28 +0800

    Add: Firestore dependency

--- app/src/main/java/com/example/huaheejqc/MainActivity.kt
+++ app/src/main/java/com/example/huaheejqc/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.huaheejqc
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var db: FirebaseFirestore;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        db = FirebaseFirestore.getInstance()
+        val docRef = db.collection("Book").document("gWjfJ36pvrtr4lA4JPCJ")
+        val TAG = "Debug"
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    Log.d(TAG, "DocumentSnapshot data: ${document.data}")
+                } else {
+                    Log.d(TAG, "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d(TAG, "get failed with ", exception)
+            }
     }
 }
