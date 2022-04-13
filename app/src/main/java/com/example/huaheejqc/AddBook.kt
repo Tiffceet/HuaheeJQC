46d2785272de003fca5cfe014ee415025fae7835
chinszesing <99196809+SzeSing@users.noreply.github.com>
2022-04-14 01:17:25 +0800

    Add: Add Book

--- app/src/main/java/com/example/huaheejqc/AddBook.kt
+++ app/src/main/java/com/example/huaheejqc/AddBook.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [AddBookManagement.newInstance] factory method to
+ * Use the [AddBook.newInstance] factory method to
  * create an instance of this fragment.
  */
-class AddBookManagement : Fragment() {
+class AddBook : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class AddBookManagement : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_book_management, container, false)
+        return inflater.inflate(R.layout.fragment_add_book, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class AddBookManagement : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment AddBookManagement.
+         * @return A new instance of fragment AddBook.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            AddBookManagement().apply {
+            AddBook().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
