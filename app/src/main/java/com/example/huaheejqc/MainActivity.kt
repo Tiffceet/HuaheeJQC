caec4c274ae61e6bdc8de2ac9f3763495236b78a
Tiffceet <zkang.loo@gmail.com>
2022-04-14 15:37:57 +0800

    Convert Login/Register Activity to Fragment and add navigation

--- app/src/main/java/com/example/huaheejqc/MainActivity.kt
+++ app/src/main/java/com/example/huaheejqc/MainActivity.kt
@@ -1,28 +1,11 @@
 package com.example.huaheejqc
 
-import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.firestore.FirebaseFirestore
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var db: FirebaseFirestore;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        db = FirebaseFirestore.getInstance()
-        val docRef = db.collection("Book").document("gWjfJ36pvrtr4lA4JPCJ")
-        val TAG = "Debug"
-        docRef.get()
-            .addOnSuccessListener { document ->
-                if (document != null) {
-                    Log.d(TAG, "DocumentSnapshot data: ${document.data}")
-                } else {
-                    Log.d(TAG, "No such document")
-                }
-            }
-            .addOnFailureListener { exception ->
-                Log.d(TAG, "get failed with ", exception)
-            }
     }
 }
