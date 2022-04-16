7d3ae57fc559cec4f1bad148f2054aa3544c0cf5
Tiffceet <zkang.loo@gmail.com>
2022-04-16 15:47:07 +0800

    Hide KB after user press login

--- app/src/main/java/com/example/huaheejqc/LoginFragment.kt
+++ app/src/main/java/com/example/huaheejqc/LoginFragment.kt
@@ -1,18 +1,22 @@
 package com.example.huaheejqc
 
 import android.app.Activity
+import android.content.Context
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.huaheejqc.databinding.FragmentLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -66,6 +70,7 @@ class LoginFragment : Fragment() {
                         val user = auth.currentUser
                         binding.loginPageStatusText.setTextColor(Color.parseColor("#00FF00"))
                         binding.loginPageStatusText.text = "Login Successful"
+                        view.hideKeyboard()
                         view.findNavController().navigate(R.id.action_loginFragment_to_mainMenu)
                     } else {
                         // If sign in fails, display a message to the user.
@@ -79,6 +84,11 @@ class LoginFragment : Fragment() {
         }
 
         return view
+    }
+
+    private fun View.hideKeyboard() {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(windowToken, 0)
     }
 
     companion object {
