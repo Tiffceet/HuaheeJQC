9b7b6f210213a866a8909ef75b516b151262a3b6
kazexu12 <60764235+kazexu12@users.noreply.github.com>
2022-04-16 11:14:13 +0800

    update a bit in reload

--- app/src/main/java/com/example/huaheejqc/ReloadInWallet.kt
+++ app/src/main/java/com/example/huaheejqc/ReloadInWallet.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.huaheejqc.databinding.ActivityMainBinding
+import  com.google.firebase.firestore.FirebaseFirestore
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +22,9 @@ class ReloadInWallet : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var db : FirebaseFirestore
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +41,8 @@ class ReloadInWallet : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_reload_in_wallet, container, false)
     }
+
+
 
     companion object {
         /**
