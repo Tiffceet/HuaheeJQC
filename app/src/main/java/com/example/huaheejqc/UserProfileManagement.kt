80fc3c347f79a7666dd4c7f8fd81d31698377130
kazexu12 <60764235+kazexu12@users.noreply.github.com>
2022-04-17 13:00:52 +0800

    update profile layout and navigation to user edit from profile

--- app/src/main/java/com/example/huaheejqc/UserProfileManagement.kt
+++ app/src/main/java/com/example/huaheejqc/UserProfileManagement.kt
@@ -5,6 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.findNavController
+import com.example.huaheejqc.databinding.FragmentLoginBinding
+import com.example.huaheejqc.databinding.FragmentUserProfileManagementBinding
+import com.google.firebase.auth.ktx.userProfileChangeRequest
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,12 +26,15 @@ class UserProfileManagement : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var _binding: FragmentUserProfileManagementBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -34,7 +43,14 @@ class UserProfileManagement : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_profile_management, container, false)
+        _binding = FragmentUserProfileManagementBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.goToEditUser.setOnClickListener{view : View ->
+            view.findNavController().navigate(R.id.action_userProfileManagement_to_userEdit)
+        }
+
+        return view
     }
 
     companion object {
