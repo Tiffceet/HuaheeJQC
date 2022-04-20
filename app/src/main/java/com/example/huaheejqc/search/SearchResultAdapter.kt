d86bd83ed9b5d19911b31c9b722b1d5bc270c3df
Tiffceet <zkang.loo@gmail.com>
2022-04-20 12:00:07 +0800

    Make search result com navigate to book details on click

--- app/src/main/java/com/example/huaheejqc/search/SearchResultAdapter.kt
+++ app/src/main/java/com/example/huaheejqc/search/SearchResultAdapter.kt
@@ -2,7 +2,9 @@ package com.example.huaheejqc.search
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.example.huaheejqc.SearchDirections
 import com.example.huaheejqc.data.SearchItem
 import com.example.huaheejqc.databinding.FragmentSearchResultComBinding
 
@@ -26,6 +28,10 @@ class SearchResultAdapter (
         val item = values[position]
         holder.bookTitle.text = item.title
         holder.bookDesc.text = item.description
+        holder.container.setOnClickListener {
+            val action = SearchDirections.actionSearchToBookDetails(item.id)
+            it.findNavController().navigate(action)
+        }
     }
 
     override fun getItemCount(): Int = values.size
@@ -34,5 +40,6 @@ class SearchResultAdapter (
         RecyclerView.ViewHolder(binding.root) {
         val bookTitle = binding.bookTitle
         val bookDesc = binding.bookDesc
+        val container = binding.searchResultComContainer
     }
 }
